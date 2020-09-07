9e7abb6482496c5c8d7271e93fdaf99107262160
Mohammad Javad Pooladkhay <m.pooladkhay@gmail.com>
2020-09-08 00:28:57 +0430

    currentuser & signout route fix

--- src/routes/currentUser.js
+++ src/routes/currentUser.js
@@ -5,12 +5,20 @@ const requireAuth = require("../middlewares/require-auth");
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
-	const user = User.findOne({ email: req.currentUser.email });
+router.get("/api/users/current", currentUser, requireAuth, async (req, res) => {
+	const user = await User.findOne({ email: req.currentUser.email });
 	if (user) {
-		res.send({ error: false, user });
+		const dataToSend = {
+			name: user.name,
+			email: user.email,
+			referedBy: user.referedBy,
+			uId: user.uId,
+			scenarioUId: user.scenarioUId,
+			points: user.points,
+		};
+		res.send({ error: false, user: dataToSend });
 	} else {
-		res.send({ error: true, msg: "Not Authorized." });
+		res.send({ error: true, message: "Not Authorized." });
 	}
 });
 
--- src/routes/signout.js
+++ src/routes/signout.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const currentUser = require("../middlewares/current-user");
 
 const router = express.Router();
 
 // signout will delete cookie and next visit will be
 // like it's user's first visit!
-router.post("/api/users/signout", (req, res) => {
-	req.session = null;
+router.post("/api/users/signout", currentUser, async (req, res) => {
+	// const scenarioForUser = await Scenario.findOne({ userUId: req.currentUser.uId });
+	// console.log(scenarioForUser);
+
+	// Generate JWT
+	const userJwt = jwt.sign(
+		{
+			uId: req.currentUser.uId,
+		},
+		process.env.JWT_KEY
+	);
+
+	// Store it on Session Object
+	req.session = { jwt: userJwt };
+
+	// req.session = null;
 	res.send({});
 });
 
