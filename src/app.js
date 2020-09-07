31f4728e52c6ba5d74865a2e96cbf5a122e26414
Mohammad Javad Pooladkhay <m.pooladkhay@gmail.com>
2020-09-07 20:11:26 +0430

    resolving CORS error

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ const assignRoute = require("./routes/assignScenario");
 
 const app = express();
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+});
+
 app.use(json());
 app.set("trust proxy", true);
 app.use(cookieSession({ signed: false }));
